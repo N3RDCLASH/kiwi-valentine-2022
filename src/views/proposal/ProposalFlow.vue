f9ad1eb7a8f6059610d3be369c47ba827dc42ba7
Joel Naarendorp <35632308+N3RDCLASH@users.noreply.github.com>
2022-02-12 23:32:04 -0300

    added spinner

--- src/views/proposal/ProposalFlow.vue
+++ src/views/proposal/ProposalFlow.vue
@@ -1,8 +1,35 @@
 // Not the most efficient way to transitions or conditional rendering 🥴
 <template>
+  <div
+    class="flex flex-col h-screen justify-center items-center w-full"
+    v-if="this.state.loading"
+  >
+    <svg
+      class="animate-spin -ml-1 mr-3 h-5 w-5 pink-text w-1/4"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        class="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        stroke-width="4"
+      ></circle>
+      <path
+        class="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
+    </svg>
+  </div>
   <transition name="fadeo" mode="in-out">
     <are-you-ready
-      v-if="!this.state.displayTimer && !this.state.readyYes"
+      v-if="
+        !this.state.loading && !this.state.displayTimer && !this.state.readyYes
+      "
       @readyYes="(state) => (this.state.displayTimer = state)"
       @readyNo="(state) => (this.state.readyNo = state)"
     ></are-you-ready>
@@ -59,7 +86,12 @@ export default {
     Modal,
   },
   setup() {
+    const loadDelay = () => {
+      setTimeout(() => (state.loading = false), 1000);
+    };
+    loadDelay();
     const state = reactive({
+      loading: true,
       readyYes: false,
       readyNo: false,
       displayTimer: false,
@@ -74,4 +106,8 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="css" scoped>
+.pink-text {
+  color: #ff7876;
+}
+</style>
