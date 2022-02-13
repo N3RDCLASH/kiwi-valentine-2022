70fbd01b63311d4e0470a72d3d9d71698beaef50
Joel Naarendorp <35632308+N3RDCLASH@users.noreply.github.com>
2022-02-12 23:12:13 -0300

    added calendar button

--- src/components/ValentineConfirmed.vue
+++ src/components/ValentineConfirmed.vue
@@ -9,6 +9,16 @@
           {{ minutes }} minutes, {{ seconds }} seconds.
         </h2>
       </vue-countdown>
+
+      <a
+        href="https://add.eventable.com/events/620867590832380019e5b550/6208675b7312ad00e0fe159f"
+        class="calendar"
+        target="_blank"
+        data-key="620867590832380019e5b550"
+        data-event="6208675b7312ad00e0fe159f"
+        data-style="on"
+        >Add to Calendar</a
+      >
     </div>
     <div class="h-1/6 p-0">
       <img src="../assets/img/wave.svg" class="bottom-0 absolute" alt="" />
@@ -16,12 +26,14 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import svg from "../assets/img/bi_calendar-check-fill.svg";
 export default {
   setup() {
-    const now = new Date();
-    const valentinesDay = new Date("14 Feb 2022");
+    const now: Date = new Date();
+    const valentinesDay: Date = new Date("14 Feb 2022");
 
+    window.localStorage.setItem("valentineConfirmed", "true");
     return {
       time: valentinesDay - now,
     };
@@ -36,5 +48,9 @@ h1 {
 h2 {
   @apply mt-5 text-center text-3xl md:text-4xl font-bold justify-self-center;
   color: #ff7876;
+}
+.calendar {
+  @apply mt-2 block w-40 text-center pt-2  text-white rounded mx-auto text-2xl;
+  background-color: #9ad14b;
 }
 </style>
