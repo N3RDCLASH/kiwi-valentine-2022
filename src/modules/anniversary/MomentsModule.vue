23029e5fd9bfa80a49754b848e2c665f3584a451
Joel Naarendorp <joelhammen11@gmail.com>
2022-09-14 11:03:51 -0300

    updated modules

--- src/modules/anniversary/MomentsModule.vue
+++ src/modules/anniversary/MomentsModule.vue
@@ -1,11 +1,17 @@
 <template>
-  <section class="flex flex-col items-center py-10">
-    <h2 class="text-[20px] md:text-[40px] lg:text-[64px]">
+  <section class="flex flex-col items-center py-10 gap-4">
+    <h2 class="text-[24px] md:text-[40px] lg:text-[64px]">
       Some momements we’ve shared.
     </h2>
-    <carousel :items-to-show="1" :autoplay="2000">
+    <carousel
+      :items-to-show="3"
+      :autoplay="2000"
+      class="bg-[url(/assets/rose-bg.png)]"
+    >
       <slide v-for="(slide, index) in moments" :key="index">
-        <div class="flex flex-col h-[30em] shadow-md rounded-md self-center">
+        <div
+          class="flex flex-col max-h-[40em] shadow-md rounded-md self-center"
+        >
           <img
             :src="`/assets/moments/${slide.url}`"
             class="h-full rounded-md"
@@ -41,6 +47,7 @@ export default defineComponent({
 <style>
 .carousel__track {
   display: flex !important;
+  gap: 2em;
 }
 .carousel__slide {
   height: inherit !important;
--- src/modules/anniversary/VideoModule.vue
+++ src/modules/anniversary/VideoModule.vue
@@ -12,19 +12,23 @@
         alt=""
       />
     </picture>
-    <video
-      :ref="
-        (el) => {
-          videoPlayer = el;
-        }
-      "
-      class="-mt-[6rem] z-0"
-      src="../../assets/video/voltzberg.mp4"
-      :autoplay="true"
-      muted
-      loop
-      preload="true"
-    ></video>
+    <div
+      class="content before:text-[20px] before:md:text-[40px] before:lg:text-[96px]"
+    >
+      <video
+        :ref="
+          (el) => {
+            videoPlayer = el;
+          }
+        "
+        class="-mt-[6rem] z-0"
+        src="../../assets/video/voltzberg.mp4"
+        :autoplay="true"
+        muted
+        loop
+        preload="true"
+      ></video>
+    </div>
   </section>
 </template>
 
@@ -46,4 +50,27 @@ export default defineComponent({
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.content {
+  position: relative;
+}
+.content video {
+  width: 100%;
+  display: block;
+}
+.content:before {
+  content: "Where it all started.";
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-family: "Cutive";
+  position: absolute;
+  background: rgba(0, 0, 0, 0.4);
+  border-radius: 5px;
+  top: -6rem;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+</style>
